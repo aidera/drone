97282711b237ec2edf5361e255fbc863b68b6dda
aidera <hope.aidera@gmail.com>
2024-09-26 17:45:33 -0400

    Responsible content

--- src/components/DroneComposition/Camera.tsx
+++ src/components/DroneComposition/Camera.tsx
@@ -1,9 +1,32 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { PerspectiveCamera as DreiPerspectiveCamera } from '@react-three/drei';
 import { PerspectiveCamera } from 'three';
 
 export default function Camera() {
   const cameraRef = useRef<PerspectiveCamera>(null);
+
+  useEffect(() => {
+    const updateCameraPosition = () => {
+      if (cameraRef.current) {
+        if (window.innerWidth <= 830) {
+          cameraRef.current.position.x = -0.5;
+        } else {
+          cameraRef.current.position.x = -4;
+        }
+      }
+    };
+
+    // Initial check
+    updateCameraPosition();
+
+    // Add event listener
+    window.addEventListener('resize', updateCameraPosition);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', updateCameraPosition);
+    };
+  }, []);
 
   return (
     <DreiPerspectiveCamera
